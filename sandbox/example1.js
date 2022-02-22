456f7e65b9d9b88fcf99446ad11457fc9228e437
patryCCio <patryk.szczerbinski@o2.pl>
2022-02-22 03:07:15 +0100

    Nauka TypeScript

--- sandbox/example1.js
+++ sandbox/example1.js
@@ -1,1 +1,25 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 console.log("Hello world!");
+var x = 2;
+var y;
+var z;
+y = "napis";
+z = true;
+z = false;
+console.log(x + " " + y);
+console.log(z);
+var xd = [3, 2, 1];
+var ys;
+xd[0] = 4;
+var zs = __spreadArray([1, 5, 7], xd, true);
+console.log(zs);
+console.log(xd.length);
+console.log("Liczba x = ".concat(x));
--- sandbox/example1.ts
+++ sandbox/example1.ts
@@ -1,6 +1,26 @@
 console.log("Hello world!");
 
-let x = 2;
-let y = 2;
+let x: number = 2;
+let y: string;
+let z: boolean;
 
-console.log(x+y);
+y = "napis";
+
+z = true;
+z = false;
+
+console.log(x+ " " + y);
+console.log(z);
+
+let xd: number[] = [3, 2, 1];
+let ys: Array<number>;
+
+xd[0] = 4;
+
+let zs = [1, 5, 7, ...xd];
+
+console.log(zs)
+
+
+console.log(xd.length);
+console.log(`Liczba x = ${x}`);
